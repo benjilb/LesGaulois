32d9110d13d749b7759b20b332aabe78d4366af3
le blanc benjamin <lbb3864a@ens.fsi>
2022-09-27 19:16:23 +0200

    Initial commit

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -4,6 +4,19 @@ public class Gaulois {
 	private String nom;
 	private int force;
 	private int effetPotion = 1;
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	public String getNom() {
+		return nom;
+	}
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
 
-
+	public static void main(String[] args) {
+		
+	}
 }
