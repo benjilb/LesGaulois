02985c3e361cf3e32f37bf3bb9b22c61361a007e
le blanc benjamin <lbb3864a@ens.fsi>
2022-09-27 19:52:21 +0200

    Initial commit

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -4,6 +4,8 @@ public class Gaulois {
 	private String nom;
 	private int force;
 	private int effetPotion = 1;
+	
+	
 	public Gaulois(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
@@ -11,10 +13,27 @@ public class Gaulois {
 	public String getNom() {
 		return nom;
 	}
-	@Override
-	public String toString() {
-		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	
+	public void parler(String texte){
+		System.out.println(prendreParole() + "< " + texte + " >");
 	}
+	
+	private String prendreParole() {
+		return "Le gaulois " + nom + " : ";
+		
+	}
+	
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la machoire de " 
+				+ romain.getNom());
+		romain.recevoirCoup(force/3);
+	}
+	
+	
+//	@Override
+//	public String toString() {
+//		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+//	}
 
 	public static void main(String[] args) {
 		
